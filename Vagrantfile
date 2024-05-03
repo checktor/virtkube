793b4b616b4418240a2e297db69f21d7c7fb78db
Christian Hecktor <christian.hecktor@detim.de>
2024-05-03 22:33:49 +0200

    Use Ubuntu 24.04 as base image

--- Vagrantfile
+++ Vagrantfile
@@ -15,7 +15,7 @@ high_availability_enabled = num_controlplanes > 1
 
 Vagrant.configure("2") do |config|
 
-    config.vm.box = "bento/ubuntu-22.04"
+    config.vm.box = "bento/ubuntu-24.04"
 
     if high_availability_enabled
         config.vm.define "controlplane_loadbalancer" do |loadbalancer|
--- test/test-deployment-and-service.sh
+++ test/test-deployment-and-service.sh
@@ -5,23 +5,37 @@ TEST_STATUS=0
 export KUBECONFIG=sync/kubeconfig
 
 # Create deployment with two replicas
-kubectl create deployment nginx --image nginx --replicas 2 > /dev/null
+kubectl create deployment nginx --image nginx:alpine --replicas 2 > /dev/null
 
-sleep 5
+sleep 20
 
-# Cheek if first pod is running
+# Check if first pod is running
 FIRST_REPLICA_STATUS=$(kubectl get pods -o jsonpath='{.items[0].status.phase}')
 if [ "$FIRST_REPLICA_STATUS" != 'Running' ]; then
   TEST_STATUS=1
   echo 'First pod of deployment is not running!'
 fi
 
-# Cheek if second pod is running
+# Check if second pod is running
 SECOND_REPLICA_STATUS=$(kubectl get pods -o jsonpath='{.items[1].status.phase}')
 if [ "$SECOND_REPLICA_STATUS" != 'Running' ]; then
   TEST_STATUS=1
   echo 'Second pod of deployment is not running!'
 fi
+
+# Create service for deployment
+kubectl expose deployment nginx --name nginx --port 80 > /dev/null
+
+# Check if deployment is reachable via service
+kubectl run curl --restart=Never --rm -i -t --image curlimages/curl -- -s -o /dev/null nginx > /dev/null
+SERVICE_STATUS=$?
+if [ $SERVICE_STATUS -ne 0 ]; then
+  TEST_STATUS=1
+  echo 'Deployment is not reachable!'
+fi
+
+# Remove service
+kubectl delete service nginx > /dev/null
 
 # Remove deployment
 kubectl delete deployment nginx --grace-period 0 > /dev/null
