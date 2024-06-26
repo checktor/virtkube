ffec03ef9b3033354aaee571a369104d1969a80a
Christian Hecktor <christian.hecktor@detim.de>
2024-06-26 17:15:27 +0200

    Use default kernel module configuration

--- script/init-cluster-node.sh
+++ script/init-cluster-node.sh
@@ -11,12 +11,6 @@ sudo swapoff -a
 # Disable swap for future sessions
 sed -i 's~/swap.img~#/swap.img~g' /etc/fstab
 
-# Load kernel modules for current session
-sudo modprobe overlay
-sudo modprobe br_netfilter
-# Load kernel modules for future sessions
-cp /vagrant/config/modules-load.d/k8s.conf /etc/modules-load.d/k8s.conf
-
 # Configure network
 cp /vagrant/config/sysctl.d/k8s.conf /etc/sysctl.d/k8s.conf
 sudo sysctl --system
@@ -34,9 +28,9 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list
 ## Kubernetes
-curl -fsSL https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes.gpg
 echo \
-  "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/ /" | \
+  "deb [signed-by=/etc/apt/keyrings/kubernetes.gpg] https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/ /" | \
    sudo tee /etc/apt/sources.list.d/kubernetes.list
 ## Fetch repository metadata
 sudo apt-get update -qq
